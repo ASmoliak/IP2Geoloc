760a5b82f40db2ad3d7e07b73b15457dac393009
Alex Smoliak <alexandersmoliak@gmail.com>
2018-12-28 21:11:41 +0200

    Added PATH adder custom action project in c#, basic argument handling and PATH adding/removing skeleton is added.

---
+++ PATH_Modifier_Custom_Action/PathManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PATH_Modifier_Custom_Action
+{
+    class PathManager
+    {
+        #region C'tor and 'dtor
+        public PathManager(string programPath)
+        {
+            _programPath = programPath;
+        }
+        #endregion
+
+        #region Public Methods
+        public void AddProgramToPath()
+        {
+            if (DoesProgramExistInPath())
+            {
+                throw new SystemException("The path already exists in the PATH env variable.");
+            }
+        }
+
+        public void RemoveProgramFromPath()
+        {
+            if (!DoesProgramExistInPath())
+            {
+                throw new SystemException("The Program Path does not exist in the PATH env variable.");
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        bool DoesProgramExistInPath()
+        {
+            bool returnValue = false;
+            return returnValue;
+        }
+        #endregion
+
+        #region Private Members
+        string _programPath;
+        #endregion
+    }
+}
---
+++ PATH_Modifier_Custom_Action/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace PATH_Modifier_Custom_Action
+{
+    class Program
+    {
+        #region Constants
+        const int ERROR_RETURN_VALUe = 1;
+        const int SUCCESS_RETURN_VALUE = 0;
+        const int THREE_ARGUMENTS = 3;
+        const string INSTALL_PATH_ARGUMENT = "--install_path";
+        const string UNINSTALL_PATH_ARGUMENT = "--uninstall_path";
+
+        #endregion
+
+        static int Main(string[] args)
+        {
+            if (args.Length != THREE_ARGUMENTS)
+            {
+                Console.WriteLine("<ERROR> There are not enough arguments to execute this program.");
+                return DoesUserContinueInstall();
+            }
+
+             
+            string path = ExtractPathFromArgs(args);
+            if (!ValidatePath(path))
+            {
+                Console.WriteLine("<ERROR> The input program path for the PATH env variable is incorrect!");
+                return DoesUserContinueInstall();
+            }
+
+
+            return SUCCESS_RETURN_VALUE;
+        }
+
+        static int DoesUserContinueInstall()
+        {
+            Console.WriteLine("<CRITICAL> Failed to add to PATH.");
+            Console.WriteLine("<QUERY> Continue installation without adding the program path to the PATH env variable? (Y/N)");
+            string answerToQuestionChar = Console.ReadKey().Key.ToString();
+            if (answerToQuestionChar == "Y")
+            {
+                return SUCCESS_RETURN_VALUE;
+            }
+            else
+            {
+                return ERROR_RETURN_VALUe;
+            }
+        }
+
+        static bool ValidatePath(string path)
+        {
+            return Uri.IsWellFormedUriString(path, UriKind.Absolute);
+        }
+
+        static string ExtractActionFromArgs(string[] args)
+        {
+            string secondArgument = args[1];
+            return secondArgument;
+        }
+
+        static string ExtractPathFromArgs(string[] args)
+        {
+            string thirdArgument = args[2];
+            return thirdArgument;
+        }
+
+    }
+}

1abe301e1756fe936e628284c2c5acb478333f0b
Alex Smoliak <alexandersmoliak@gmail.com>
2019-01-01 18:28:22 +0200

    Created a Google Test project, created a basic single unit test, and added a "Unit Test" configuration option that switches the IP2Geoloc tool to a static library to make unit tests work.

---
+++ Tool-Tests/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "../IP2Geoloc/ProgramArgumentsParser.h"
+#include "../IP2Geoloc/IPtoGeolocConverter.h"
+#include "../IP2Geoloc/GeolocRequestor.h"
+#include "../IP2Geoloc/GeolocParser.h"

23de335415c921f924f41e1c2ee6aa54fba92e84
Alex Smoliak <alexandersmoliak@gmail.com>
2019-01-12 09:48:24 +0200

    Added a thorough, but poorly written unit test on conflicting arguments, difficult to scale, this needs improving.

--- Tool-Tests/src/ProgramArgumentsParserTests.cpp
+++ Tool-Tests/src/ProgramArgumentsParserTests.cpp
@@ -38,3 +38,37 @@ TEST(ProgramArgumentsParser_Tests, Self_Result_Validity)
 
 	EXPECT_TRUE(result.resolve_self) << "The settings struct's resolve self boolean was not set to true on correct input";
 }
+
+TEST(ProgramArgumentsParser_Tests, Conflicting_Option_Detection)
+{
+	const char *conflicting_arguments1[] = { PROGARM_PATH, SELF_RESOLVE_ARGUMENT, IP_ARGUMENT };
+	const char *conflicting_arguments2[] = { PROGARM_PATH, SELF_RESOLVE_ARGUMENT, HELP_ARGUMENT };
+	const char *conflicting_arguments3[] = { PROGARM_PATH, IP_ARGUMENT,  SELF_RESOLVE_ARGUMENT };
+	const char *conflicting_arguments4[] = { PROGARM_PATH, IP_ARGUMENT, HELP_ARGUMENT };
+	const char *conflicting_arguments5[] = { PROGARM_PATH, HELP_ARGUMENT, IP_ARGUMENT };
+	const char *conflicting_arguments6[] = { PROGARM_PATH, HELP_ARGUMENT, SELF_RESOLVE_ARGUMENT };
+	int three_arguments = 3;
+	const char *conflicting_arguments7[] = { PROGARM_PATH, HELP_ARGUMENT, SELF_RESOLVE_ARGUMENT, IP_ARGUMENT };
+	const char *conflicting_arguments8[] = { PROGARM_PATH, HELP_ARGUMENT, IP_ARGUMENT, SELF_RESOLVE_ARGUMENT };
+	const char *conflicting_arguments9[] = { PROGARM_PATH, IP_ARGUMENT, SELF_RESOLVE_ARGUMENT, HELP_ARGUMENT };
+	const char *conflicting_arguments10[] = { PROGARM_PATH, IP_ARGUMENT, HELP_ARGUMENT, SELF_RESOLVE_ARGUMENT };
+	const char *conflicting_arguments11[] = { PROGARM_PATH, SELF_RESOLVE_ARGUMENT, HELP_ARGUMENT, IP_ARGUMENT };
+	const char *conflicting_arguments12[] = { PROGARM_PATH, SELF_RESOLVE_ARGUMENT, IP_ARGUMENT, HELP_ARGUMENT };
+	int four_arguments = 4;
+
+	std::string error_string = "The argument parser did not throw on conflicting arguments";
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments1)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments2)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments3)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments4)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments5)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(three_arguments, conflicting_arguments6)) << error_string;
+
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments7)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments8)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments9)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments10)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments11)) << error_string;
+	EXPECT_ANY_THROW(ProgramArgumentsParser(four_arguments, conflicting_arguments12)) << error_string;
+}
+
